1488f228f089eba470f0dd8a8e82d8b740e55484
unknown <pornthip.wong@gmail.com>
2012-12-16 22:47:37 +0700

    update mongo connect

--- public/javascript/mongorest_service.js
+++ public/javascript/mongorest_service.js
@@ -2,7 +2,7 @@ var app = angular.module('mongorest_service', ['ngResource']);
 
 app.factory('Project', function($resource) {
   //var Project = $resource('http://www.db.grad.nu.ac.th/apps/mongodb/databases/projectplan/collections/project/:document', {   
-  var Project = $resource('query/project/:id', {     
+  var Project = $resource('/query/project/:id', {     
     id: '@id'
   },
   {update: { method:'PUT' }});
