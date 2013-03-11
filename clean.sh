ebe96fb3b8a18afee8093d5226c2086e4b6935fc
kphongph <kphongph@gmail.com>
2013-03-11 17:24:30 +0700

    removing node_modules

---
+++ clean.sh
@@ -0,0 +1,1 @@
+curl -v -H "Content-Type: application/json" -X POST -d '{"name":"grad-project","user":"pornthipw"}' http://10.10.20.75:9002/drones/grad-project/clean
---
+++ stop.sh
@@ -0,0 +1,1 @@
+curl -v -H "Content-Type: application/json" -X POST -d '{"stop":{"name":"grad-project"}}' http://10.10.20.75:9002/drones/grad-project/stop
