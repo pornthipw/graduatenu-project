0e3c78c3e02288be73631b6b62c1fb0bfd90ad6a
kphongph <kphongph@gmail.com>
2014-06-04 14:21:13 +0700

    update 4/6/2014

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -702,7 +702,7 @@ function ProjectListWarningByYearController($scope, GradDB,$routeParams, Project
 
         //niew gedeelte
         var options = {
-           colors: ["#FE9A2E", "#21610B", "#0B173B", "#DF0101", "#aaeeee", "#ff0066", "#eeaaee","#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+           colors: [],
           chart: {
             renderTo: 'container1',
             defaultSeriesType: 'spline'
@@ -712,6 +712,7 @@ function ProjectListWarningByYearController($scope, GradDB,$routeParams, Project
         $("#change").click(function(){
           if ($("#list").val() == "A")
           {
+           colors: ["#FE9A2E", "#21610B", "#0B173B", "#DF0101", "#aaeeee", "#ff0066", "#eeaaee","#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
             options.series = [{
                 type: 'pie',
                 name: 'คิดเป็น',
