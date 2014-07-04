27cd7ca3492c231e3dbe159d20bdddeff08f2f5a
kphongph <kphongph@gmail.com>
2014-07-04 14:59:29 +0700

    update

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -320,6 +320,7 @@ function ProjectListController($scope, $routeParams, Project, User, Logout) {
   $scope.current_year = $routeParams.year;
   //Project.query({query:'{"type":"post_project", "year":"'+$routeParams.year+'"}'}, function(project_list) {    
   Project.query({query:'{"type":"post_project", "year":"'+$routeParams.year+'"}'}, function(project_list) {    
+    $scope.project_list = project_list;
     console.log("test"); 
   });
 }
