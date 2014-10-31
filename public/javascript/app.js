e8dd5e3919add23d1d21909661c05a08e7d21e42
pornthipw <pornthip.wong@gmail.com>
2014-10-31 08:45:07 +0700

    update

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1449,7 +1449,7 @@ function YearListController($scope, GradDB,$location, $timeout,$routeParams,Proj
     $location.path('/projects/'+year+'/dept');
   }
   $scope.filter4 = function(year) {
-    $location.path('/task/create');
+    $location.path('/task/create/'+year);
   }
   $scope.filter5 = function(year) {
     $location.path('/project/list/'+year);
